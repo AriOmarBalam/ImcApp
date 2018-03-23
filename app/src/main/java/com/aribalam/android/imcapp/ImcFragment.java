ed82bab8f1f2af29b6ce4bfc4cdd3c30f00c6a1a
AriOmarBalam <14470221@itcampeche.edu.mx>
2018-03-22 22:56:02 -0600

    Version inicial del proyecto ImcApp. No incluye manejo de eventos.

---
+++ app/src/main/java/com/aribalam/android/imcapp/ImcFragment.java
@@ -0,0 +1,26 @@
+package com.aribalam.android.imcapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Omar Balam on 22/03/2018.
+ */
+
+public class ImcFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_imc,container,false);
+        return v;
+    }
+}
